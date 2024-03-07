3b1fa71549fb43fdbd2ede6d6ea0c1bb7ee840b4
Haris Brulic <haris.brulic@student.um.si>
2024-03-07 22:31:30 +0100

    first build

---
+++ src/test/java/harisbrulicita2024/microservice1/Microservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package harisbrulicita2024.microservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
